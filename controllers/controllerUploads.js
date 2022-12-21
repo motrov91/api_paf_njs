daef2ebc3dae455e8ffb694c42f84ded4ff2b2b1
Manuel Ramirez <manuelramirez@MacBook-Pro-de-Manuel.local>
2022-12-20 23:58:22 -0500

    changes test

--- controllers/controllerUploads.js
+++ controllers/controllerUploads.js
@@ -78,49 +78,57 @@ const updateImageProductCloud = async (req, res) => {
         })
     }
 
-    //Clean images
-    if(existProduct.img){
-        //TODO : Clean image server
-        //Delete image in server
-        // const __filename = fileURLToPath(import.meta.url);
-        // const __dirname = path.dirname(__filename);
+    try {
 
-        // const pathImage = path.join( __dirname, '../uploads/', 'imgs', existProduct.img ); 
+         //Clean images
+        if(existProduct.img){
+            //TODO : Clean image server
+            //Delete image in server
+            // const __filename = fileURLToPath(import.meta.url);
+            // const __dirname = path.dirname(__filename);
 
-        // if(fs.existsSync( pathImage )){
-        //     fs.unlinkSync( pathImage );
-        // }
+            // const pathImage = path.join( __dirname, '../uploads/', 'imgs', existProduct.img ); 
 
-        const nameArr = existProduct.img.split('/');
-        const reference = nameArr[ nameArr.length - 1 ];
-        const [ public_id ] = reference.split('.');
+            // if(fs.existsSync( pathImage )){
+            //     fs.unlinkSync( pathImage );
+            // }
 
-        cloudinary.uploader.destroy( public_id );
+            const nameArr = existProduct.img.split('/');
+            const reference = nameArr[ nameArr.length - 1 ];
+            const [ public_id ] = reference.split('.');
+
+            cloudinary.uploader.destroy( public_id );
+        }
+
+        const { tempFilePath } = req.files.archivo;
+        const { secure_url } = await cloudinary.uploader.upload( tempFilePath )
+        existProduct.img = secure_url;
+
+        await existProduct.save()
+
+        const product = await Product.findByPk(id ,{   
+            include: [
+                { model: User.scope('deletePassword') }
+            ]
+        });
+
+
+        //Upload image 
+        // const nameFile = await uploadFileHL(req.files, undefined, 'imgs');
+
+        // existProduct.img = nameFile;
+
+        // await existProduct.save()
+
+        // console.log('Actualización existosa')
+
+        return res.status(200).json(product);
+        
+    } catch (error) {
+        console.log(error)
     }
 
-    const { tempFilePath } = req.files.archivo;
-    const { secure_url } = await cloudinary.uploader.upload( tempFilePath )
-    existProduct.img = secure_url;
-
-    await existProduct.save()
-
-    const product = await Product.findByPk(id ,{   
-        include: [
-            { model: User.scope('deletePassword') }
-        ]
-    });
-
-
-    //Upload image 
-    // const nameFile = await uploadFileHL(req.files, undefined, 'imgs');
-
-    // existProduct.img = nameFile;
-
-    // await existProduct.save()
-
-    // console.log('Actualización existosa')
-
-    return res.status(200).json(product);
+   
 
 }
 
